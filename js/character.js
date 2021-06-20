f53afa0b9ef9a46e6d914523ff78004d03949f88
Adam <adam.a.lohnes@gmail.com>
2021-06-20 08:22:34 -0700

    Create JS framework

---
+++ js/character.js
@@ -0,0 +1,17 @@
+var version = '0.0.1';
+var name, gender, height, weight, availPts, str, int, agi, con, firstColor, secondColor;
+var skinTone, hairColor, hairStyle, faceShape, facialExpression, facialHair;
+
+console.log('Character Builder v' + version);
+
+function onLoad() {
+
+}
+
+function updateStat(id) {
+
+}
+
+function updatePreview() {
+
+}
