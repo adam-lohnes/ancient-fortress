a0f5afdaf05f238bf61cab71f8ca40e6cce2c160
Adam <adam.a.lohnes@gmail.com>
2021-06-21 14:33:34 -0700

    Implement options-viewer functionality

--- js/character.js
+++ js/character.js
@@ -1,6 +1,15 @@
+var charOptions = document.getElementById('character-options');
+
 var version = '0.0.1';
 var name, gender, height, weight, availPts, str, int, agi, con, firstColor, secondColor;
 var skinTone, hairColor, hairStyle, faceShape, facialExpression, facialHair;
+
+var skinColors = ['beige', 'tan', 'chocolate', 'brown', 'saddlebrown'];
+var hairColors = ['yellow', 'orange', 'brown', 'black', 'blue'];
+var hairStyles = ['bald', 'buzzed', 'short', 'medium', 'long'];
+var faceShapes = ['triangle', 'round', 'oval', 'boxy', 'square', 'rectangular'];
+var expressions = ['bubbly', 'happy', 'serious', 'angry', 'bored'];
+var facialHairs = ['none', 'moustache', 'handlebar', 'soul-patch', 'goatee', 'chinstrap', 'short-beard', 'medium-beard', 'long-beard'];
 
 console.log('Character Builder v' + version);
 
@@ -10,6 +19,28 @@ function onLoad() {
 
 function updateStat(id) {
 
+}
+
+function showPreviewOptions(tab) {
+  charOptions.innerHTML = '';
+  var optionsTable = document.createElement('table');
+  var options = document.createElement('tr');
+
+  for (var i=0; i<tab.length; i++) {
+    var option = document.createElement('td');
+    option.classList.add('character-option');
+
+    if ( tab === skinColors || tab === hairColors ) {
+      option.style.backgroundColor = tab[i];
+    } else {
+      option.innerHTML = tab[i];
+    }
+
+    options.appendChild(option);
+  }
+
+  optionsTable.appendChild(options);
+  charOptions.appendChild(optionsTable);
 }
 
 function updatePreview() {
